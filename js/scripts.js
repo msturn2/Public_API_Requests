bc3840de5ae79a40a6f832852be109681f4f3605
msturn2 <82980444+msturn2@users.noreply.github.com>
2021-06-14 09:26:57 -0700

    Refactor functions
    Cleaned code up as it was sloppy in places.  Indexing of .card and .modal-info-container divs should allow search and modal window scrolling.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
-document.querySelector("body").style.background = "lightblue";
-const gallery = document.getElementById('gallery');
-const employees = [];
+const URL = "https://randomuser.me/api/1.3/?results=12&nat=us&exc=id,login,registered&noinfo";
+const body = document.querySelector("body");
+body.style.background = "lightblue";
+const gallery = document.getElementById("gallery");
+let employeeDataArray = [];
 
 /**-----------------------------------
  *          FETCH FUNCTIONS
@@ -10,7 +12,8 @@ async function fetchData(url) {
     try {
         const response = await fetch(url);
         const res = await checkStatus(response);
-        return res.json();
+        const result = await res.json();
+        return result.results
     } catch (error) {
         return console.error("There was a problem fetching the data", error);
     }
@@ -26,45 +29,26 @@ function checkStatus(response) {
 }
 
 
-fetchData('https://randomuser.me/api/1.3/?results=12&nat=us&exc=id,login,registered')
-.then(data => {
-    
-    employees.push(...data.results);
-    generateCard(employees)
-    generateModalElement();
-    displayCardModal(employees);
-    buttonToClose();
-});
+fetchData(URL)
+    .then(generateCard)
+    .then(genModCon)
 
 /**-----------------------------------
  *          HELPER FUNCTIONS
  -------------------------------------*/
 
-/**
-    Search markup: You can use the commented out markup below as a template for your search feature, but you must use JS to create and 
-    append it to `search-container` div.
-
-    IMPORTANT: Altering the arrangement of the markup and the 
-    attributes used may break the styles or functionality.
-
-    <form action="#" method="get">
-        <input type="search" id="search-input" class="search-input" placeholder="Search...">
-        <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
-    </form>
-*/
-
-
 function generateCard(data) {
-    data.forEach(result => {
+    employeeDataArray = data;
+    employeeDataArray.forEach((result, index)  => {
         const picture = result.picture.large;
         const name = `${result.name.first} ${result.name.last}`;
         const email = result.email;
         const cityState = `${result.location.city}, ${result.location.state}`;
     
         const htmlCard = `
-            <div class="card">
+            <div class="card active" data-index="${index}" data-employee="${name.first} ${name.last}">
                 <div class="card-img-container">
-                    <img class="card-img" src="${picture}" alt="${name}">
+                    <img class="card-img" src="${picture}" alt="profile picture">
                 </div>
                 <div class="card-info-container">
                     <h3 id="name" class="card-name cap">${name}</h3>
@@ -73,67 +57,52 @@ function generateCard(data) {
                 </div>
             </div>
         `;
-        gallery.insertAdjacentHTML('beforeEnd', htmlCard);
+        gallery.insertAdjacentHTML("beforeEnd", htmlCard);
     })
 }
 
 
-function generateModalElement() {
-    const htmlModalElement = `
-        <div class="modal-container">
-            <div class="modal">
-                <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-            </div>
-        </div>
-    `;
-    gallery.insertAdjacentHTML("afterEnd", htmlModalElement);
-    const modContainer = document.querySelector(".modal-container");
-    modContainer.style.display = "none";
-    modContainer.style.background = "orange";
+function genModCon() {
+    const genModalDiv = `<div class=modal-container></div>`;
+    gallery.insertAdjacentHTML("afterEnd", genModalDiv);
+    const modalDiv = document.querySelector(".modal-container");
+    modalDiv.style.display = "none";
 }
 
 
-function generateModal(data) {
-    const picture = data.picture.large;
-    const name = `${data.name.first} ${data.name.last}`;
-    const email = data.email;
-    const cityState = `${data.location.city}, ${data.location.state}`;
-    const city = data.location.city;
-    const address = `${data.location.street.number} ${data.location.street.name}, ${cityState} ${data.location.postcode}`;
-    const bDay = formatBDay(data.dob.date);
-    const cell = formatCell(data.cell);
-
+const displayModal = (index) => {
+    let { picture, name, email, location: { city, street, state, postcode
+    }, dob, cell} = employeeDataArray[index];
+    
+    const fullName = `${name.first} ${name.last}`;
+    const address = `${street.number} ${street.name}, ${city}, ${state} ${postcode}`;
+    const bDay = formatBDay(dob.date);
+    const cellNum = formatCell(cell);
+    
     const htmlModal = `
-        <div class="modal-info-container">
-            <img class="modal-img" src="${picture}" alt="profile picture">
-            <h3 id="name" class="modal-name cap">${name}</h3>
-            <p class="modal-text">${email}</p>
-            <p class="modal-text cap">${city}</p>
-            <hr>
-            <p class="modal-text">Mobile: ${cell}</p>
-            <p class="modal-text">${address}</p>
-            <p class="modal-text">Birthday: ${bDay}</p>
-        </div>
-        <div class="modal-btn-container">
-            <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
-            <button type="button" id="modal-next" class="modal-next btn">Next</button>
-        </div>
+        <div class="modal">
+            <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+                <div class="modal-info-container" data-index="${index}">
+                    <img class="modal-img" src="${picture.large}" alt="profile picture">
+                    <h3 id="name" class="modal-name cap">${fullName}</h3>
+                    <p class="modal-text">${email}</p>
+                    <p class="modal-text cap">${city}</p>
+                    <hr>
+                    <p class="modal-text">Mobile: ${cellNum}</p>
+                    <p class="modal-text">${address}</p>
+                    <p class="modal-text">Birthday: ${bDay}</p>
+                </div>
+            </div>
+            <div class="modal-btn-container">
+                <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+                <button type="button" id="modal-next" class="modal-next btn">Next</button>
+            </div>
     `;
-    const modDiv = document.querySelector(".modal");
-    modDiv.insertAdjacentHTML("beforeEnd", htmlModal);
+    const modCon = document.querySelector(".modal-container");
+    modCon.innerHTML = htmlModal;
+    modCon.style.display = "";
+    modCon.style.background = "orange";
 }
-
-
-// function nextPrevButtons() {
-//     const htmlButtons = `
-//         <div class="modal-btn-container">
-//             <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
-//             <button type="button" id="modal-next" class="modal-next btn">Next</button>
-//         </div>
-//     `;
-//     const modInfo = document.querySelector(".modal-info.container");
-//     modInfo.insertAdjacentHTML("afterEnd", htmlbuttons);
-// }
 
 /**-----------------------------------
  *        FORMATTING FUNCTIONS
@@ -152,38 +121,18 @@ function formatCell(cell) {
  *          EVENT LISTENERS
  -------------------------------------*/
 
-function displayCardModal(data) {
-    const cards = document.querySelectorAll(".card");
-    for (let i = 0; i < cards.length; i++) {
-        cards[i].addEventListener("click", () => {
-            generateModal(data[i]);
-            gallery.nextElementSibling.style.display = "";
-        });
+gallery.addEventListener("click", (e) => {
+    if (gallery !== e.target) {
+        const clicked = e.target.closest(".card");
+        const idx = clicked.getAttribute("data-index");
+        displayModal(idx);
     }
-}
+});
 
 
-function prevButton(data) {
-    const prev = document.querySelector("#modal-prev");
-    prev.addEventListener("click", (e)=> {
-        const cards = document.querySelectorAll
-    });
-}
-
-
-// function nextPrev(data) {
-//     const ntPvButton = document.querySelectorAll("#modal");
-// }
-
-
-function buttonToClose() {
-    const buttonClose = document.querySelector("#modal-close-btn");
-        
-    buttonClose.addEventListener("click", () => {
-        const modInfoCon = document.querySelector(".modal-info-container")
-        buttonClose.parentNode.parentNode.style.display = "none";
-        modInfoCon.nextElementSibling.remove();
-        modInfoCon.remove();
-    });
-}
-console.log(employees);
+body.addEventListener("click", (e) => {
+    if (e.target.textContent === "X") {
+        const modalCon = document.querySelector(".modal-container");
+        modalCon.style.display = "none";
+    }
+});
